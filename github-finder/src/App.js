39c846a94ba0a797df647a3fe77c898d01ce6263
Mhamzahhameed <hamzahraja11@gmail.com>
2022-05-25 16:46:43 +0500

    Create github-finder app
    .1 Created github-finder app in the new repository and copied some created files and code from the previous app source code( that was created in the previous repository)

---
+++ github-finder/src/App.js
@@ -0,0 +1,65 @@
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
+import axios from 'axios';
+import './App.css';
+
+class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+    alert: null
+  }
+
+  // This function is called from the search component by passing props up
+  searchUsers = async (text) => {
+  this.setState( { loading: true} )
+  const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`) 
+  this.setState( { users: res.data.items, loading: false} )
+  } 
+
+  // This function is called from the Search component to clear users from state
+  clearUsers = () => this.setState({users : [], loading: false})
+
+  // This fuction is called from the Search component to raise an alert for empty text field search
+  setAlert = (msg, type) => {
+    this.setState({alert: {msg, type} })
+    // Remove the alert message after 5 seconds
+    setTimeout(() => this.setState({alert: null }), 5000)
+  }
+
+  render () {
+    const { users, loading, alert} = this.state
+    return (
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert alert={alert} />
+            <Switch>
+              <Route exact path='/' render={ props => (
+                <Fragment>
+                 <Search searchUsers={this.searchUsers} 
+                         clearUsers= {this.clearUsers}
+                         showClear={users.length > 0 ? true : false }
+                         setAlert={this.setAlert}
+                  />
+                  <Users loading={loading} users={users} />
+                </Fragment>
+              )}>
+
+              </Route>
+            </Switch>
+
+         </div>
+       </div>
+      </Router>
+    );
+  }
+}
+
+
+export default App;
